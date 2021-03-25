3d281f477f4514e8bdaaba412c6d7be3514b4e1e
Artur da Silva Rezende <arturrezende4@gmail.com>
2021-03-25 15:07:00 -0300

    structured project and ready for coding

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+//Font Awesome
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
+
+//Favicon
+import "../assets/favicon-32x32.png";
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  devtool: false,
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [miniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: "file-loader",
+        options: { name: "assets/[name].[ext]" },
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2?)$/i,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new miniCssExtractPlugin({
+      filename: "css/style.css",
+    }),
+    new webpack.SourceMapDevToolPlugin({}),
+  ],
+};
